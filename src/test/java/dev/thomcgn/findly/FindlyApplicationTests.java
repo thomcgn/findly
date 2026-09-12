04628e36e3fe1b5720e6a73228ef33e45d7dbf04
Tom Kramer <kramer.th@outlook.de>
2026-09-12 15:29:04 +0000

    Initial commit

---
+++ src/test/java/dev/thomcgn/findly/FindlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.findly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindlyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
